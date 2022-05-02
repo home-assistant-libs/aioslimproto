39107cc4f081dcea9e417b8ecb4519652b2a010b
Paulus Schoutsen <balloob@gmail.com>
2022-05-02 11:28:31 -0700

    Convert JSONRPCMessage command params to string

---
+++ tests/test_json_rpc.py
@@ -0,0 +1,15 @@
+"""Tests for JSON RPC."""
+
+from aioslimproto import json_rpc
+
+
+def test_msg_instantiation():
+    """Test instantiating a JSON RPC message."""
+    msg = json_rpc.JSONRPCMessage.from_json(
+        id=1, method="method", params=[3, ["command", 5]]
+    )
+    assert msg.id == 1
+    assert msg.method == "method"
+    assert msg.player_id == "3"
+    assert msg.command_args == ["5"]
+    assert msg.command_str == "command 5"
