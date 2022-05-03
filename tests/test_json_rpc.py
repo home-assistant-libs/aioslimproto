5dc59ee4210db4675e8f811f221826834b894f62
Paulus Schoutsen <balloob@gmail.com>
2022-05-02 22:35:07 -0700

    Test for command with no args

--- tests/test_json_rpc.py
+++ tests/test_json_rpc.py
@@ -13,3 +13,13 @@ def test_msg_instantiation():
     assert msg.player_id == "3"
     assert msg.command_args == ["5"]
     assert msg.command_str == "command 5"
+
+    # No command args
+    msg = json_rpc.JSONRPCMessage.from_json(
+        id=1, method="method", params=[3, ["command"]]
+    )
+    assert msg.id == 1
+    assert msg.method == "method"
+    assert msg.player_id == "3"
+    assert msg.command_args == []
+    assert msg.command_str == "command"
